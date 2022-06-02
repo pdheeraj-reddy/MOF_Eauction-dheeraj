f6eb82af09b8471619ccb0117ac0b8a2ef29f61f
sathya-26 <sathya@seyali.com>
2022-06-02 17:26:57 +0530

    envi update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   ruId: '1d18eecc-6f4e-476f-bec3-f2c2c94521f6',
   idmLoginURL: "https://souq.etimad.sa/irj/servlet/prt/portal/prtroot/pcd!3aportal_content!2fmof.gov.sa.f_mof_base!2fmof.gov.sa.f_iviews!2fmof.gov.sa.i_auction",
   idmLogoutUrl: "https://souq.etimad.sa/sap/public/bc/icf/logoff?sap-client=100",
-  idmHomeUrl: "https://loginpp.etimad.sa/",
+  idmHomeUrl: "https://login.etimad.sa/",
   apiAuctionURL : "/internal/v1/e-auction/auctions",
   apiFilenetURL : "/internal/v1/e-auction/files",
   apiCommiteeURL : "/internal/v1/e-auction/committees",
