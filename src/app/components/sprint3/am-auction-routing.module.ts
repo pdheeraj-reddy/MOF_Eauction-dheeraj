e1c7a12cd27e5d0a9daf47db8c27fcc9c3dd206c
btharachandar <100573601+btharachandar@users.noreply.github.com>
2022-07-05 18:20:08 +0530

    Adding css to shared components

--- src/app/components/sprint3/am-auction-routing.module.ts
+++ src/app/components/sprint3/am-auction-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AmAuctionComponent } from './bidder/am-auction/am-auction.component';
+import { AuctionDetailsComponent } from './shared/auction-details/auction-details.component';
 
-const routes: Routes = [{ path: '', component: AmAuctionComponent }];
+const routes: Routes = [
+  { path: '', component: AmAuctionComponent },
+  { path: 'auction-details', component: AuctionDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
