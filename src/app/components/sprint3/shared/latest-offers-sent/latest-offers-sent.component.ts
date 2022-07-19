8f7d211c2bf5b5d197d04c60c41fc5ba7115dcad
btharachandar <100573601+btharachandar@users.noreply.github.com>
2022-07-19 15:05:33 +0530

    Calculate date in latest offers send and primary awarding UI changes

--- src/app/components/sprint3/shared/latest-offers-sent/latest-offers-sent.component.ts
+++ src/app/components/sprint3/shared/latest-offers-sent/latest-offers-sent.component.ts
@@ -78,43 +78,46 @@ export class LatestOffersSentComponent implements OnInit {
 
 
   getsince(dat: string, time: string) {
-    let str = "18.07.2022 17:00:00"
-    const [dateComponents, timeComponents] = str.split(' ');
-    const [day, month, year] = dateComponents.split('.');
-    const [hours, minutes, seconds] = timeComponents.split(':');
+    // const [dateComponents, timeComponents] = str.split(' ');
+    // console.log("🎯TC🎯 ~ file: latest-offers-sent.component.ts ~ line 83 ~ dat", dat ,time);
+    const [day, month, year] = dat.split('.');
+    const [hours, minutes, seconds] = time.split(':');
     const date = new Date(+year, +month - 1, +day, +hours, +minutes, +seconds);
+    // console.log("🎯TC🎯 ~ file: latest-offers-sent.component.ts ~ line 86 ~ date", date);
     // console.log(Date.now()-date.getSeconds());
-
-    return this.getTime(new Date(Date.now() -  date.getSeconds()));
+    // console.log(date);
+    // let currentDate = new Date(Date.now());
+    // console.log("Date passed",new Date(date.getTime()));
+    // console.log("Current date",currentDate);
+    return this.getTime(date);
   }
-
-
   getTime(date: any): any {
-
+    console.log("🎯TC🎯 ~ file: latest-offers-sent.component.ts ~ line 92 ~ date", date);
+    
     let dt: any = new Date();
     let seconds = Math.floor(dt - date) / (1000)
 
     var interval = seconds / 31536000;
 
     if (interval > 1) {
-      return Math.floor(interval) + " years";
+      return Math.floor(interval) + " years ago";
     }
     interval = seconds / 2592000;
     if (interval > 1) {
-      return Math.floor(interval) + " months";
+      return Math.floor(interval) + " months "+"ago";
     }
     interval = seconds / 86400;
     if (interval > 1) {
-      return Math.floor(interval) + " days";
+      return Math.floor(interval) + " days "+ "ago";
     }
     interval = seconds / 3600;
     if (interval > 1) {
-      return Math.floor(interval) + " hours";
+      return Math.floor(interval) + " hours ago";
     }
     interval = seconds / 60;
     if (interval > 1) {
-      return Math.floor(interval) + " minutes";
+      return Math.floor(interval) + " minutes ago";
     }
-    return Math.floor(seconds) + " seconds";
+    return Math.floor(seconds) + " seconds ago";
   }
 }
