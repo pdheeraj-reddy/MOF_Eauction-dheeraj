528404de940ca318aef234dc1bdb404824374b7d
Ratnesh Soni <sratnesh@kaartech.com>
2022-08-08 17:27:01 +0530

    sprint 3 tracker issue

--- src/app/components/sprint3/shared/latest-offers-sent/latest-offers-sent.component.ts
+++ src/app/components/sprint3/shared/latest-offers-sent/latest-offers-sent.component.ts
@@ -10,15 +10,17 @@ import { AucModeratorService } from '../../services/auc-moderator.service';
 })
 export class LatestOffersSentComponent implements OnInit {
   currentLang: string | null;
-
-  constructor(private api: AucModeratorService,
-    public PaginationServc: PaginationSortingService) { }
-
   biddet: any[] = [];
   @Input() aucId: any;
   @Input() pageno: any;
   PageNo: string;
   textDir: any;
+  pagelimit: number = 8;
+  pageRangeForAttach: any;
+  constructor(private api: AucModeratorService,
+    public PaginationServc: PaginationSortingService) { }
+
+
   ngOnInit(): void {
 
     this.PageNo = this.pageno;
@@ -40,7 +42,6 @@ export class LatestOffersSentComponent implements OnInit {
       if (newLang == 'ar') {
         this.currentLang = newLang;
         this.textDir = false;
-        // // // console.log("🎯TC🎯 ~ file: auction-details.component.ts ~ line 72 ~ textDir", this.textDir);
       }
       else {
         this.textDir = true;
@@ -49,51 +50,37 @@ export class LatestOffersSentComponent implements OnInit {
     }
 
   }
-  getBidDetails(pageno:any) {
-    // if (pagenu) {
-    //   this.api.getLatestBiddetails(this.aucId, pagenu).subscribe((res => {
-    //     console.log(res['body']['d']['results'][0]['pagetoaucbiddernav']['results']);
-
-    //     this.biddet = res['body']['d']['results'][0]['pagetoaucbiddernav']['results']
-
-    //     this.PaginationServc.resetSorting();
-
-    //   }))
-    // }
-    // else {
-    //   this.api.getLatestBiddetails(this.aucId, this.PageNo).subscribe((res => {
-    //     console.log(res);
-
-    //     this.PaginationServc.resetSorting();
-
-    //   }))
-    // }
-    this.api.getLatestBiddetails(this.aucId, pageno).subscribe((res:any)=>{
+  getBidDetails(pageno: any) {
+    this.api.getLatestBiddetails(this.aucId, pageno).subscribe((res: any) => {
       this.biddet = res['body']['d']['results'][0]['pagetoaucbiddernav']['results'];
-      console.log("🎯TC🎯 ~ file: latest-offers-sent.component.ts ~ line 73 ~ this.biddet", this.biddet);
+      console.log("this.biddet", this.biddet);
       this.PaginationServc.resetSorting();
+      this.navigateToPage(1);
+      console.log('this.PaginationServc: ', this.PaginationServc);
     });
 
   }
 
+  navigateToPage(pageNoVal: number) {
+    this.PaginationServc.setPagerValues(this.biddet.length, this.pagelimit, pageNoVal);
+    this.pageRangeForAttach = {
+      rangeStart: pageNoVal == 1 ? 0 : ((pageNoVal - 1) * this.pagelimit),
+      rangeEnd: pageNoVal == 1 ? (this.pagelimit - 1) : ((pageNoVal - 1) * this.pagelimit) + (this.pagelimit - 1),
+      pages: this.PaginationServc.pages,
+      currentPage: this.PaginationServc.currentPage,
+      totalPages: this.PaginationServc.totalPages,
+    }
+  }
+
 
   getsince(dat: string, time: string) {
-    // const [dateComponents, timeComponents] = str.split(' ');
-    // // console.log("🎯TC🎯 ~ file: latest-offers-sent.component.ts ~ line 83 ~ dat", dat ,time);
     const [day, month, year] = dat.split('.');
     const [hours, minutes, seconds] = time.split(':');
     const date = new Date(+year, +month - 1, +day, +hours, +minutes, +seconds);
-    // // console.log("🎯TC🎯 ~ file: latest-offers-sent.component.ts ~ line 86 ~ date", date);
-    // console.log(Date.now()-date.getSeconds());
-    // console.log(date);
-    // let currentDate = new Date(Date.now());
-    // console.log("Date passed",new Date(date.getTime()));
-    // console.log("Current date",currentDate);
     return this.getTime(date);
   }
+
   getTime(date: any): any {
-    // console.log("🎯TC🎯 ~ file: latest-offers-sent.component.ts ~ line 92 ~ date", date);
-    
     let dt: any = new Date();
     let seconds = Math.floor(dt - date) / (1000)
 
@@ -104,11 +91,11 @@ export class LatestOffersSentComponent implements OnInit {
     }
     interval = seconds / 2592000;
     if (interval > 1) {
-      return Math.floor(interval) + " months "+"ago";
+      return Math.floor(interval) + " months " + "ago";
     }
     interval = seconds / 86400;
     if (interval > 1) {
-      return Math.floor(interval) + " days "+ "ago";
+      return Math.floor(interval) + " days " + "ago";
     }
     interval = seconds / 3600;
     if (interval > 1) {
