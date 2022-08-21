29336d2f92d50e0848555461eb9c8c61d0e5a429
Ratnesh Soni <sratnesh@kaartech.com>
2022-08-21 11:36:50 +0530

    json file changes

--- src/app/components/sprint3/home/home-routing.module.ts
+++ src/app/components/sprint3/home/home-routing.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuctionDetailComponent } from './auction-detail/auction-detail.component';
 import { AuctionComponent } from './auction/auction.component';
-import { HomeComponent } from './home.component';
 import { LandingComponent } from './landing/landing.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/landing', pathMatch: 'full' },
-  { path: '', component: HomeComponent },
   { path: 'landing', component: LandingComponent },
   { path: 'auction', component: AuctionComponent },
   { path: 'auction-detail', component: AuctionDetailComponent },
