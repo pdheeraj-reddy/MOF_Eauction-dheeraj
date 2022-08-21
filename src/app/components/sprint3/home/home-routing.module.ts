3c83c673a5f5c7ca5f086c74125582b521d3c533
Ratnesh Soni <sratnesh@kaartech.com>
2022-08-21 18:30:48 +0530

    landing page changes

--- src/app/components/sprint3/home/home-routing.module.ts
+++ src/app/components/sprint3/home/home-routing.module.ts
@@ -5,7 +5,7 @@ import { AuctionComponent } from './auction/auction.component';
 import { LandingComponent } from './landing/landing.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/landing', pathMatch: 'full' },
+  { path: '', redirectTo: 'landing', pathMatch: 'full' },
   { path: 'landing', component: LandingComponent },
   { path: 'auction', component: AuctionComponent },
   { path: 'auction-detail', component: AuctionDetailComponent },
